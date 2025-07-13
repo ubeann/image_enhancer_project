29f0f5fae16302a8989050c260ea95e4f277776a
ubeann <76271600+ubeann@users.noreply.github.com>
2025-07-13 12:59:00 +0700

    fix: resize the file ouput

--- deblur_gan/inference.py
+++ deblur_gan/inference.py
@@ -22,8 +22,9 @@ def deblur_image(image_path: str, save_path: str = None) -> Image.Image:
         Image.Image: The deblurred image as a PIL Image object.
     """
 
-    # Open the input image and ensure it's in RGB format
+    # Open the input image and ensure it's in RGB format and get its original size
     image = Image.open(image_path).convert("RGB")
+    original_size = image.size
 
     # Resize the image to 256x256, matching the model's expected input size
     image = image.resize((256, 256))
@@ -42,6 +43,9 @@ def deblur_image(image_path: str, save_path: str = None) -> Image.Image:
 
     # Convert the array back to a PIL Image
     result = Image.fromarray(pred.astype("uint8"))
+
+    # Resize back to original
+    result = result.resize(original_size)
 
     # Save the image if a save path is provided
     if save_path:
